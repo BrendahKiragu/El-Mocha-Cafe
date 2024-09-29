5a1f1bfdbc9e196ad7386b2e0985fabb0fd7f516
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-09-29 22:40:21 +0300

    fetches data from db.json and displays data, initializes app with the fetched data

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const navLink = document.querySelectorAll(".nav-link");
 const sections = document.querySelectorAll(".section");
+const menuItem = document.getElementById("item");
 
+//toggles visibility of nav links
 navLink.forEach((link) => {
   link.addEventListener("click", function (e) {
     e.preventDefault();
@@ -14,3 +16,45 @@ navLink.forEach((link) => {
     document.getElementById(targetSection).style.display = "block";
   });
 });
+
+//renders the fetch data as an item card
+function renderItem(coffee) {
+  const itemCard = document.createElement("div");
+  itemCard.innerHTML = `
+    <img class="icon" src="${coffee.img}">
+    <article class="item">
+      <h3>${coffee.name}</h3>
+      <p class="flavor">${coffee.description}</p> 
+      <p class="price">Price:$ ${coffee.price}</p>
+    </article>
+    <hr class="item-hr">
+  `;
+  menuItem.appendChild(itemCard);
+}
+
+// fetch
+function fetchData() {
+  fetch("http://localhost:3000/menu")
+    .then((res) => res.json())
+    .then((data) => {
+      const coffeeHeader = document.createElement("h2");
+      coffeeHeader.textContent = "COFFEE";
+      menuItem.appendChild(coffeeHeader);
+      data.coffee.map((coffee) => renderItem(coffee));
+
+      const hr = document.createElement("h2");
+      hr.innerHTML = `
+      <hr>
+      <h2> DESSERT </h2>
+      `;
+      menuItem.appendChild(hr);
+
+      data.desserts.map((dessert) => renderItem(dessert));
+    });
+}
+
+function initialize() {
+  fetchData();
+}
+
+initialize();
